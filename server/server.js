ded4d27030931157f7fcaa92f252a2b1c28d5503
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-09-25 23:59:04 +0530

    add

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const  express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.get('/', (req,res) => res.send('Hello Nitesh'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", (req, res) => res.send("Hello Nitesh"));
 
-app.listen(8080, () => {console.log('Server started on http://localhost:8080')})
+app.listen(8080, () => {
+  console.log("Server started on http://localhost:8080");
+});
