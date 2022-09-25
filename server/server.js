a5d4649e40a1fbf82b8bdf4cbbe95bfd0f51d7bc
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-09-25 23:49:48 +0530

    add nodemoon

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.get('/', (req,res) => res.send('hello'))
+app.get('/', (req,res) => res.send('Hello'))
 
 app.listen(8080, () => {console.log('Server started on http://localhost:8080')})
