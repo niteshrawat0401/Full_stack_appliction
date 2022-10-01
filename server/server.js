ec671975bdb726d9c55eaf8b874913b88e16eccf
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-01 17:46:40 +0530

    fix server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const authRouter= require("./Routes/auth")
-const connect= require("./db/db")
-const cors= require("cors");
+const authRouter = require("./Routes/auth");
+const connect = require("./db/db");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: ["http://localhost:3000"]
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => res.send("Hello Nitesh"));
 
-app.listen(8080, async() => {
-  connect
+app.listen(8080, async () => {
+  connect;
   console.log("Server started on http://localhost:8080");
 });
