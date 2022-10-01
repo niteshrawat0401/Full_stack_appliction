3f4a1a968b777e92d7772faead45fa61c78a43a9
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-01 17:48:48 +0530

    delete env

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,11 +1,11 @@
-const {Schema,model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const userSchema= new Schema({
-    userName: String,
-    passWord: String,
-    email: String,
-    mobile: String
-})
+const userSchema = new Schema({
+  userName: String,
+  passWord: String,
+  email: String,
+  mobile: String,
+});
 
-const User=model("User", userSchema);
-module.exports= User;
+const User = model("User", userSchema);
+module.exports = User;
