2ce6ba4f8a70132755f627d18b88af83c24bc724
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-10-01 17:48:10 +0530

    db

--- Server/db/db.js
+++ Server/db/db.js
@@ -1,2 +1,4 @@
-const mongoose= require("mongoose");
-module.exports= mongoose.connect(`mongodb+srv://niteshrawat:niteshrawat@cluster0.v82owbn.mongodb.net/nitesh?retryWrites=true&w=majority`);
+const mongoose = require("mongoose");
+module.exports = mongoose.connect(
+  `mongodb+srv://niteshrawat:niteshrawat@cluster0.v82owbn.mongodb.net/nitesh?retryWrites=true&w=majority`
+);
